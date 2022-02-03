a886860e11d5dadde314328e664ff20bdc41921d
divyanshu bist <divyanshu_fake@gmail.com>
2022-02-03 21:55:43 +0530

    puzzle are generating properly,work on gui left

--- solver.py
+++ solver.py
@@ -6,60 +6,74 @@ row_nos=[]
 col_nos=[]
 block_nos=[]
 
-for i in range(row):
-    row_nos.append(0)
-    col_nos.append(0)
-    block_nos.append(0)
 
-def no_generator(i,j):
-    block_row=int(i/3)*3
-    block_col=int(j/3)*3
-    k=block_row+int(block_col/3)
-    while(True):
-	    rand_no=random.randrange(0,9)
-	    if((row_nos[i]&(1<<rand_no))==0 and (col_nos[j]&(1<<rand_no))==0 and (block_nos[k]&(1<<rand_no))==0):
-                row_nos[i]=row_nos[i]|(1<<rand_no)   
-                col_nos[j]=col_nos[j]|(1<<rand_no)
-                block_nos[k]=block_nos[k]|(1<<rand_no)
- #               print(i,row_nos[i],j,col_nos[j],k,block_nos[k])
-                break
-    return rand_no
-
-
-for i in range(row):
+for i in range(0,row):
     cols=[]
-    rand_no=random.randrange(2,7)
-    for j in range(col):
-        put=random.randrange(2)
-        if(put==1 and rand_no>0):
-                no=no_generator(i,j);
-                cols.append(no)
-                #print("generated",i,j,no,"\n")
-                rand_no=rand_no-1
-        else:
-#            print(i,j,0,"\n")
-            cols.append(0)
+    for j in range(0,col):
+        cols.append(0)
     sudoku.append(cols)
 
 
 
-        
-for r in sudoku: 
-    print(r)
+def check(row,col,val):
+    for i in range(0,9):
+    	if(sudoku[i][col]==val):
+           return False
+    for i in range(0,9):
+        if(sudoku[row][i]==val):
+           return False
+    block_row=int(row/3)*3
+    block_col=int(col/3)*3
+    for i in range(block_row,block_row+3,1):
+        for j in range(block_col,block_col+3,1):
+           if(sudoku[i][j]==val):
+            	return False               
+    return True   
+    
 
 
-def solver(sudoku,row,col):
+def generator(sudoku,row,col,lst):
     if(row==9):
-        for r in sudoku:
-            print(r,end=" ")
-        print()
+        global done
+        done=True
+        return 
     new_row=row
     new_col=col+1
-    if(col==8):
-        new_row=row+1
-        new_col=0
-    for val  in range(1,9,1):
-        if(possible):
-            sudoku[row][col]=val
-            solver(sudoku,new_row,new_col)
-            sudoku[row][col]=0
+    length=len(lst)
+    remain=[]
+    while(length>0):
+    	val=random.choice(lst)
+    	if(check(row,col,val)==True):
+    		sudoku[row][col]=val
+    		lst.remove(val)
+    		new_list=lst.copy()
+    		new_list.extend(remain)
+    		if(col==8):
+    			new_list=[x for x in range(1,10)]
+    			new_row=row+1
+    			new_col=0
+    		generator(sudoku,new_row,new_col,new_list)
+    		if(done==True):
+    			return
+    		else:    			 
+    			sudoku[row][col]=0
+	    		remain.append(val)
+    	else:
+    		lst.remove(val)
+    		remain.append(val)
+    	length=length-1
+
+done=False 
+
+lst=[x for x in range(1,10)]          	
+generator(sudoku,0,0,lst)            
+
+for i in range(row):
+	rand_no=random.randrange(2,7)
+	for j in range(col):
+		pos=random.randrange(0,2)
+		if(pos==0 and rand_no>0):
+			sudoku[i][j]=0
+for r in sudoku:
+    print(r)	
+            
